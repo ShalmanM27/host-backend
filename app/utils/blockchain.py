7dc1eef875c3c1a8fd7d3711214b35833788f974
ShalmanM27 <148774091+ShalmanM27@users.noreply.github.com>
2025-10-08 22:03:19 +0530

    Delete app/utils directory

--- app/utils/blockchain.py
+++
@@ -1,9 +0,0 @@
-from web3 import Web3
-from config import *
-
-# Connect to Hela Testnet
-w3 = Web3(Web3.HTTPProvider(HELA_RPC))
-account = w3.eth.account.from_key(PRIVATE_KEY)
-
-def get_contract(address, abi):
-    return w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
